d90da04afd1977d05721b353bb7deb4b1f8f4adc
KaMBIare <nikolaev_maksim05@mail.ru>
2024-04-18 21:04:21 +0300

    fimaly

---
+++ Infrastructure/ApplicationContext.cs
@@ -0,0 +1,46 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Reservation> Reservations { get; set; }
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //подключаемся к бд
+        //ConnectToPostgres(optionsBuilder);
+        ConnectToSqlite(optionsBuilder);
+
+
+       
+    }
+    private void ConnectToSqlite(DbContextOptionsBuilder optionsBuilder)
+    {
+        string stringConnection;
+        using (StreamReader streamReader = new StreamReader("TextConnectionSqlite.txt"))
+        {
+            stringConnection = streamReader.ReadToEnd();
+        }
+
+        optionsBuilder.UseSqlite(stringConnection);
+    }
+
+    private void ConnectToPostgres(DbContextOptionsBuilder optionsBuilder)
+    {
+        string stringConnection;
+        using (StreamReader streamReader = new StreamReader("TextConnectionPostgres.txt"))
+        {
+            stringConnection = streamReader.ReadToEnd();
+        }
+
+        optionsBuilder.UseNpgsql(stringConnection);
+    }
+}
